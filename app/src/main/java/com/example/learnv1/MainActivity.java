122d192bd9ff177d271f459b656eb2c1145dfa25
Luis Quijano <luis5.quijano5@gmail.com>
2023-11-02 13:40:03 -0600

    Changed the regular keyboard to numeric keyboard

--- app/src/main/java/com/example/learnv1/MainActivity.java
+++ app/src/main/java/com/example/learnv1/MainActivity.java
@@ -14,7 +14,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import android.content.Context;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 
+//main game
 public class MainActivity extends AppCompatActivity {
 
     int counter = 0;
@@ -55,34 +60,14 @@ public class MainActivity extends AppCompatActivity {
         txtResult.setText(result.getValue()+"");
         txtEquation.setText(result.getRep());
 
+        //only activate submit button when theres num in both
         edtA.addTextChangedListener(valuesTextWatcher);
         edtZ.addTextChangedListener(valuesTextWatcher);
+
+        // For moving from editText to editText
+        setEditorActionListener(edtA, edtZ); // editText1 to editText2
+        setEditorActionListener(edtZ, edtA); // editText2 to editText1
     }
-
-    public void onBackPressed() {
-        stopTimer();
-        finish(); // Finish the activity when the back button is pressed
-    }
-
-    private TextWatcher valuesTextWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            String aField = edtA.getText().toString().trim();
-            String zField = edtZ.getText().toString().trim();
-
-            submitValues.setEnabled( containsNumber(aField) && containsNumber(zField) );
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-
-        }
-    };
 
     public void Generate(View view)
     {
@@ -102,8 +87,13 @@ public class MainActivity extends AppCompatActivity {
         txtResult.setText(result.getValue()+"");
         txtEquation.setText(result.getRep());
 
+        //only activate submit button when theres num in both
         edtA.addTextChangedListener(valuesTextWatcher);
         edtZ.addTextChangedListener(valuesTextWatcher);
+
+        // For moving from editText to editText
+        setEditorActionListener(edtA, edtZ); // editText1 to editText2
+        setEditorActionListener(edtZ, edtA); // editText2 to editText1
     }
 
     public void Submit(View view) {
@@ -132,6 +122,55 @@ public class MainActivity extends AppCompatActivity {
             lost("Incorrect!");
         }
     }
+
+    //this makes the change from edittext to edittext automatic
+    private void setEditorActionListener(final EditText editText, final EditText nextEditText) {
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE || (event != null && event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+                    if (nextEditText != null) {
+                        nextEditText.requestFocus();
+                    } else {
+                        // Hide the keyboard if there is no next EditText
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void onBackPressed() {
+        stopTimer();
+        finish(); // Finish the activity when the back button is pressed
+    }
+
+    private TextWatcher valuesTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String aField = edtA.getText().toString().trim();
+            String zField = edtZ.getText().toString().trim();
+
+            submitValues.setEnabled( containsNumber(aField) && containsNumber(zField) );
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String text = s.toString();
+            if (!text.isEmpty() && !text.matches("-?\\d*")) {
+                // If the text is not a valid number, remove non-numeric characters
+                s.replace(0, s.length(), text.replaceAll("[^\\d-]", ""));
+            }
+        }
+    };
 
     public void lost(String wayOfLosing){
         stopTimer();
